95f26ac4d140e976b5dae8c293b1ee68fc06ce5d
Koras02 <wjdgh0727@gmail.com>
2025-08-10 10:57:39 +0900

    Part1.DataTypes

---
+++ P1.DataTypes/1.ScalaVariable.pl
@@ -0,0 +1,7 @@
+my $name = "Sally";
+my $age = 30;
+my $hobby = "reading";
+my $blog = "https://sallysblog.example.com";
+
+print "$name is $age years old and enjoys $hobby, Check out her blog at $blog.\n"
+// "Sally is 30 years old and enjoys reading, Check out her blog at https://sallysblog.example.com."
