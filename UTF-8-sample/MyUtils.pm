04b541b94f31496feb914e52aed4b84f597269c7
Koras02 <wjdgh0727@gmail.com>
2025-08-12 22:35:57 +0900

    Part2.CommentIndents

--- UTF-8-sample/MyUtils.pm
+++ UTF-8-sample/MyUtils.pm
@@ -3,3 +3,13 @@ use strict;
 use warnings;
 use utf8;
 use Exporter 'import';
+
+# 외부에서 사용할 함수 목록
+our @EXPORT_OK = qw(save_utf8);
+
+
+# UTF-8 파일 저장 함수
+sub save_utf8 {
+    my ($file_name, $content) = @_;
+    open my $fh, '>:encoding(utf8)', $file_name
+}
